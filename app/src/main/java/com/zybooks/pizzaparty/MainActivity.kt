8eeecdbc042ffc4f6f117b2fc30c5a976a21355a
fahriyildiz <fyildiz2080@sdsu.edu>
2023-05-10 18:13:31 -0600

    Added comments and radio group code

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -3,38 +3,61 @@ package com.zybooks.pizzaparty
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.zybooks.pizzaparty.ui.theme.PizzaPartyTheme
 import kotlin.math.ceil
 
 const val SLICES_PER_PIZZA = 8
 
+/**
+ * Main activity
+ *
+ * This class calculates the number of slices for
+ * each person.
+ * @author Fahri Yildiz
+ */
 class MainActivity : ComponentActivity() {
+
     private lateinit var numAttendEditText: EditText
     private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
 
+    /**
+     * Creates the view
+     * @property savedInstanceState is the instance state
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Stores the Edit Text in a variable
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        // Stores the Text View in a variable
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+        // Stores the Radio Group in a variable
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
     }
 
+    /**
+     * This function is activated when the user clicks on
+     * the "calcuate" button
+     * @property view
+     */
     fun calculateClick(view: View) {
+        // Gets the value entered into Edit Text
         val numAttendStr = numAttendEditText.text.toString()
+        // Parse the value to integer
         val numAttend = numAttendStr.toInt()
-        val slicesPerPerson = 4
+        // Determines which radio button was pressed to set slicesPerPerson
+        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> 2
+            R.id.medium_radio_button -> 3
+            else -> 4
+        }
+        // Calculates the total number of pizzas
         val totalPizzas = ceil(numAttend * slicesPerPerson /
                 SLICES_PER_PIZZA.toDouble()).toInt()
+        // Shows the total pizzas in the Text View
         numPizzasTextView.text = "Total pizzas: $totalPizzas"
     }
 }
